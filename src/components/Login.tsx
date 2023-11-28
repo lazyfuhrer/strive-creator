4d3297090b20d167ab582badf95d0b8b618be59e
Biswarghya Biswas <biswasbiswarghya@gmail.com>
2023-11-28 15:49:57 +0530

    feat: login

---
+++ src/components/Login.tsx
@@ -0,0 +1,72 @@
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  Button,
+  Heading,
+  Text,
+  useColorModeValue,
+  Link,
+} from '@chakra-ui/react'
+
+export default function Login() {
+  return (
+    <Flex
+      minH={'100vh'}
+      align={'center'}
+      justify={'center'}
+      bg={useColorModeValue('gray.50', 'gray.800')}>
+      <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
+        <Stack align={'center'}>
+          <Heading fontSize={'4xl'}>Login</Heading>
+        </Stack>
+        <Box
+          rounded={'lg'}
+          bg={useColorModeValue('white', 'gray.700')}
+          boxShadow={'lg'}
+          p={8}>
+          <Stack spacing={4}>
+            <FormControl id="email">
+              <FormLabel>Email address</FormLabel>
+              <Input type="email" />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" />
+            </FormControl>
+            <Stack spacing={5}>
+
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}>
+                <Checkbox>Remember me</Checkbox>
+                <Text color={'blue.400'}>Forgot password?</Text>
+              </Stack>
+
+              <Button
+                bg={'blue.400'}
+                color={'white'}
+                _hover={{
+                  bg: 'blue.500',
+                }}>
+                Continue
+              </Button>
+
+              <Stack>
+                <Text align={'center'}>
+                  Don't have an account? <Link color={'blue.400'}>Signup</Link>
+                </Text>
+              </Stack>
+
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
+import { Tomorrow } from 'next/font/google'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
+const tomorrow = Tomorrow({ weight: ['300', '400', '500', '600'], subsets: ['latin'] })
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+    <ChakraProvider>
+      <main className={tomorrow.className}>
+        <Component {...pageProps} />
+      </main>
+    </ChakraProvider>
+  );
+};
