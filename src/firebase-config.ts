ea09ef12204bf3e4d9a6fd646065c1475ff7f8a3
Biswarghya Biswas <biswasbiswarghya@gmail.com>
2023-12-16 15:15:19 +0530

    fix: env vars for firebase

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD3MdfZmgdDriI3D2nsfugAM4wcxrinNfQ",
-  authDomain: "strive-creator.firebaseapp.com",
-  projectId: "strive-creator",
-  storageBucket: "strive-creator.appspot.com",
-  messagingSenderId: "53206111703",
-  appId: "1:53206111703:web:321e37e88b40afb4592792",
-  measurementId: "G-2019VZXQ3B"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.firebaseapp.com`,
+  projectId: process.env.EXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
