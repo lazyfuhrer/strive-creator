1ad783a3fe3bd87cbd2ec66c038c74c2270a39f6
lazyfuhrer <iambiswarghya@gmail.com>
2024-02-10 12:44:30 +0530

    feat: added gtag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,7 @@ import { configureChains, createConfig, WagmiConfig } from 'wagmi';
 import { polygonMumbai } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
+import { GoogleTagManager } from '@next/third-parties/google'
 
 const tomorrow = Tomorrow({ weight: ['300', '400', '500', '600'], subsets: ['latin'] });
 
@@ -47,6 +48,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <ChakraProvider toastOptions={{ defaultOptions: { position: 'top-right' } }}>
           <main className={tomorrow.className}>
             <Component {...pageProps} />
+            <GoogleTagManager gtmId="G-84RP516MXT" />
           </main>
         </ChakraProvider>
       </RainbowKitProvider>
