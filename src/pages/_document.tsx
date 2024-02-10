7505f72e40c952c57adb02ab7a26238a864020af
lazyfuhrer <iambiswarghya@gmail.com>
2024-02-10 13:51:33 +0530

    fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,21 +1,10 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import { Html, Head, Main, NextScript } from 'next/document';
 import theme from '@/styles/theme';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      {/* <!-- Google tag (gtag.js) --> */}
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-ED21VRT398"/>
-      <Script id="my-script">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-ED21VRT398');
-        `}
-      </Script>
       <Head />
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
